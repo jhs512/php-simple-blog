09dc4b2aa49018b9500a14602ec6b7c06b824787
jhs512 <jangka512@gmail.com>
2020-12-06 11:53:35 +0900

    관리자 기능 추가

--- public/article_list.php
+++ public/article_list.php
@@ -19,17 +19,21 @@ while ( $row = mysqli_fetch_assoc($rs) ) {
 <section class="section section-article-list con-min-width">
   <div class="con">
     <div>
-        번호 : <a class="hover-underline" href="article_detail.php?id=<?=$row['id']?>"><?=$row['id']?></a>
+      번호 : <a class="hover-underline" href="article_detail.php?id=<?=$row['id']?>"><?=$row['id']?></a>
     </div>
     <div>
-        날짜 : <?=$row['regDate']?>
+      날짜 : <?=$row['regDate']?>
     </div>
     <div>
-        제목 : <a class="hover-underline" href="article_detail.php?id=<?=$row['id']?>"><?=$row['title']?></a>
+      제목 : <a class="hover-underline" href="article_detail.php?id=<?=$row['id']?>"><?=$row['title']?></a>
     </div>
     <div>
-        <a href="javascript:;" onclick="if ( confirm('삭제하시겠습니까?') ) { location.replace('/do_delete_article.php?id=<?=$row['id']?>'); };" class="underline">삭제</a>
-        <a href="/modify_article.php?id=<?=$row['id']?>" class="underline">수정</a>
+      <?php if ( App__actorCanDelete($row) ) { ?>
+      <a href="javascript:;" onclick="if ( confirm('삭제하시겠습니까?') ) { location.replace('/do_delete_article.php?id=<?=$row['id']?>'); };" class="underline">삭제</a>
+      <?php } ?>
+      <?php if ( App__actorCanModify($row) ) { ?>
+      <a href="/modify_article.php?id=<?=$row['id']?>" class="underline">수정</a>
+      <?php } ?>
     </div>
     <hr />
   </div>
